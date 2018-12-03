1a2dc9072c5eaba5d37516e5a776418c2f8d9b57
Jeppe <j3ppes@gmail.com>
2018-12-03 10:22:44 +0100

    lade till scannern

--- Create.java
+++ Create.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Create{
+  Scanner scan = new Scanner();
   static ArrayList<UserList> user = new ArrayList<UserList>();
   UserList UsL = new UserList(userName, userPassword);
-  
+
 
 private static void createUser(Scanner scan){
   System.out.println("Enter Username: ");
