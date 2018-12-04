9ec10767f5f97bc59cff8dae610568e4452f25b1
gso1910 <gustav.bj@gmail.com>
2018-12-04 22:14:55 +0100

    Cleaned up a bit

--- TTK_Main.java
+++ TTK_Main.java
@@ -1,44 +1,43 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
+
+
+
+
+
+
+
 public class TTK_Main {
 
   // Public boolean for logged in status
   public static boolean logged = false;
-  // Private boolean for menu loops
-  private static boolean looping = true;
   // Private string for currently logged in username
   private static String username = "-1";
 
 
   private void welcomeMessage() {
-    if (!username.equals("-1")) {
+    if (logged) {
       System.out.println("Welcome " + username + "!" + '\n');
     } else {
       System.out.println("You are not logged in." + '\n');
     }
-
   }
 
   private void displayOptions() {
     if (!logged) {
       System.out.println("1. Login");
+    } else {
+      System.out.println("1. Logout");
     }
-
     System.out.println("2. Create new user");
-
-    if (logged) {
-      System.out.println("3. Logout");
-    }
-    else {
-      System.out.println("3. Quit");
-    }
+    System.out.println("3. Quit");
   }
 
-  private boolean loginOrCreateUser(Scanner scan) {
+  private void loginOrCreateUser(Scanner scan) {
 
     int userOption = 0;
-    looping = true;
+    boolean looping = true;
 
     while (looping) {
 
@@ -46,8 +45,9 @@ public class TTK_Main {
 
       try {
         userOption = scan.nextInt();
-        if (userOption == 1) {
-          System.out.println('\n' + "[call method to login user]");
+
+        if (userOption == 1 && !logged) { // User selected "login"
+          System.out.println('\n' + "[call method to login user]" + '\n');
           // System.out.print('\n' + "Username: ");
           // Call the login method:
           // 1. Sends 'username' as parameter to be checked;
@@ -55,15 +55,15 @@ public class TTK_Main {
           // username = Login.login(username);
           // Add if-statement here? if (username != "-1") { logged = true } ?
           logged = true; // Set user as logged in
-          System.out.println("You are logged in." + '\n');
-        } else if (userOption == 2) {
-          // Create.create();
-          System.out.println('\n' + "[call method to create user]");
-        } else if (userOption == 3 && logged) { // User selected "logout"
-          System.out.println("Loggin out ...");
+          welcomeMessage();
+        } else if (userOption == 1 && logged) { // User selected "logout"
+          System.out.println("Logging out ...");
           logged = false;
           System.out.println("You are logged out." + '\n');
-        } else if (userOption == 3 && !logged) { // User selected "quit"
+        } else if (userOption == 2) { // User selected "create new user"
+          // Create.create();
+          System.out.println('\n' + "[call method to create user]" + '\n');
+        } else if (userOption == 3) { // User selected "quit"
           System.out.println("Goodbye.");
           looping = false;
         } else {
@@ -75,7 +75,6 @@ public class TTK_Main {
         scan.next();
       }
     } // while closure
-    return looping;
   }
 
   // --------------------------------------------------------------------------
@@ -85,10 +84,7 @@ public class TTK_Main {
     TTK_Main ttk_main = new TTK_Main();
     Scanner scan = new Scanner(System.in);
 
-    while (looping) {
-      ttk_main.welcomeMessage();
-      looping = ttk_main.loginOrCreateUser(scan);
-    }
+    ttk_main.loginOrCreateUser(scan);
 
     scan.close();
 
