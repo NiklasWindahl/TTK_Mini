db44c74eb876988a973f201a4711979866896ed4
gso1910 <gustav.bj@gmail.com>
2018-12-05 11:17:46 +0100

    Users class

--- Users.java
+++ Users.java
@@ -1,24 +1,29 @@
 import java.util.ArrayList;
 
 public class Users{
-  private ArrayList<String> validUsers = new ArrayList<String>();
-  private ArrayList<String> validPasswords = new ArrayList<String>();
+  public ArrayList<String> validUsers = new ArrayList<String>();
+  public ArrayList<String> validPasswords = new ArrayList<String>();
 
-  public void UserList(){
+
+  public void createUsers() {
+    validUsers.add("user1");
+    validUsers.add("user2");
+    validUsers.add("user3");
+    validUsers.add("user4");
+    validUsers.add("user5");
+
+    validPasswords.add("password1");
+    validPasswords.add("password2");
+    validPasswords.add("password3");
+    validPasswords.add("password4");
+    validPasswords.add("password5");
+  }
+
+/*
+  public void Users(){
       validUsers = new ArrayList();
       validPasswords = new ArrayList();
-
-      validUsers.add("user1");
-      validUsers.add("user2");
-      validUsers.add("user3");
-      validUsers.add("user4");
-      validUsers.add("user5");
-
-      validPasswords.add("password1");
-      validPasswords.add("password2");
-      validPasswords.add("password3");
-      validPasswords.add("password4");
-      validPasswords.add("password5");
   }
+*/
 
 }
