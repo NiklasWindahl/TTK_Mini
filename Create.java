a0c10de1a2582a833d8c0949812192416f4a4ae0
Jeppe <j3ppes@gmail.com>
2018-12-07 13:05:35 +0100

    Lagt till boolean för att kolla om user skapas

--- Create.java
+++ Create.java
@@ -11,19 +11,41 @@ Scanner scan = new Scanner(System.in);
   // static ArrayList<String> password = new ArrayList<String>();
 
 
-public void createUser(){
-
-  // ska anropa Users
-
- System.out.println("Enter Username: ");
+public String nameInput(){
  String userName = scan.next();
- UR.validUsers.add(userName);
 
- System.out.println("Enter Password: ");
- String userPassword = scan.next();
- UR.validPasswords.add(userPassword);
+   for (int i = 0; i < UR.validUsers.size(); i++) {
+     if (userName.equals(UR.validUsers.get(i))) {
+       userName = "-1";
+     }
+   }
 
- System.out.println("User: " + userName + " has been created!       please choose next action");
+ //UR.validUsers.add(userName);
+ return userName;
+}
+
+public String passInput(){
+  String userPassword = scan.next();
+  //UR.validPasswords.add(userPassword);
+  return userPassword;
+}
+
+public boolean createUser(String userName, String userPassword){
+  int userArraySizeBefore = UR.validUsers.size();
+  UR.validUsers.add(userName);
+  int userArraySizeAfter = UR.validUsers.size();
+
+  int passwordArraySizeBefore = UR.validPasswords.size();
+  UR.validPasswords.add(userPassword);
+  int passwordArraySizeAfter = UR.validPasswords.size();
+
+  if (userArraySizeBefore == userArraySizeAfter || passwordArraySizeBefore == passwordArraySizeAfter){
+    return false;
+  }else{
+    return true;
+  }
+
+
 
 }
 
@@ -36,14 +58,14 @@ public void createUser(Scanner scan){
   System.out.println("User: " + this.userName + " has been created!       please choose next action");
   Users.validUsers.add(this.userName);
   Users.validPasswords.add(this.userPassword);
-} */
+}
 
   public static void main(String[] args) {
     Create CR = new Create();
     System.out.println(UR.validUsers.size());
     CR.createUser();
     System.out.println(UR.validPasswords.size());
-
+    */
 
 
 }
